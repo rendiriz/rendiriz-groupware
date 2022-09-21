e178ecfe616b2bb85538c3856f9dddf0587900ab
Rendi Rizki <rendirizkir@gmail.com>
2022-09-22 00:00:08 +0700

    fix: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,12 @@ import Layout from '@/layouts/default/Layout';
 const Home: NextPage = () => {
   const { data: session } = useSession();
 
-  let accessToken = '';
   const greeting = !session ? 'Hello!' : `Hello ${session?.user?.name}!`;
-
-  if (session) {
-    accessToken = session.token as string;
-  }
 
   return (
     <Layout>
       <div className="mx-auto max-w-7xl px-4 my-12">
         <h1 className="text-5xl">{greeting}</h1>
-        <pre>{JSON.stringify(session)}</pre>
-        <pre>{accessToken}</pre>
       </div>
     </Layout>
   );
